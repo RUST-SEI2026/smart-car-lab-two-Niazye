75bbc7432cabfc3c17fcd136aa1bcd675eefb444
Niazye <niazye@outlook.com>
2026-05-17 21:48:26 +0800

    test:倒车转向的测试

--- executor/tests/executor_test.rs
+++ executor/tests/executor_test.rs
@@ -233,3 +233,34 @@ mod backward_move_tests {
         assert_eq!(expected_pose, executor.query());
     }
 }
+
+mod backward_turn_tests {
+    use super::*;
+
+    #[test]
+    fn should_return_facing_s_given_command_is_bl_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        // when
+        executor.execute("B");
+        executor.execute("L");
+        // then
+        let expected_pose = Pose::new(0, 0, 'S');
+        assert_eq!(expected_pose, executor.query());
+    }
+
+    #[test]
+    fn should_return_facing_w_given_command_is_br_and_facing_is_e() {
+        // given
+        let original_pose = Pose::new(0, 0, 'E');
+        let mut executor = Executor::with_pose(original_pose);
+        // when
+        executor.execute("B");
+        executor.execute("R");
+        // then
+        let expected_pose = Pose::new(0, 0, 'W');
+        assert_eq!(expected_pose, executor.query());
+    }
+}
+
