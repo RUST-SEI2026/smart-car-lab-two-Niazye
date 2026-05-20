61e44507a209bf2ba288a2052c653fa721fe027f
Niazye <niazye@outlook.com>
2026-05-21 00:20:52 +0800

    feat: 添加倒车和加速功能的实现

--- executor/src/executor.rs
+++ executor/src/executor.rs
@@ -3,18 +3,52 @@ use crate::pose::Pose;
 
 pub struct Executor {
     pose: Pose,
+    is_backward: bool,
+    is_fast: bool,
 }
 
 impl Executor {
     pub fn with_pose(pose: Pose) -> Self {
-        Executor { pose }
+        Executor {
+            pose,
+            is_backward: false,
+            is_fast: false,
+        }
     }
 
     pub fn execute(&mut self, cmds: &str) {
         for cmd in cmds.chars() {
+            if (self.is_fast && (cmd == 'M' || cmd == 'L' || cmd == 'R')) {
+                if self.is_backward {
+                    self.pose.step(-1);
+                } else {
+                    self.pose.step(1);
+                }
+            }
             match cmd {
-                'M' => self.pose.step(1),
-                'L' | 'R' => self.pose.turn_direction(cmd),
+                'M' => {
+                    if self.is_backward {
+                        self.pose.step(-1);
+                    } else {
+                        self.pose.step(1);
+                    }
+                }
+                'L' => {
+                    if self.is_backward {
+                        self.pose.turn_direction('R');
+                    } else {
+                        self.pose.turn_direction('L');
+                    }
+                }
+                'R' => {
+                    if self.is_backward {
+                        self.pose.turn_direction('L');
+                    } else {
+                        self.pose.turn_direction('R');
+                    }
+                }
+                'B' => self.is_backward = !self.is_backward,
+                'F' => self.is_fast = !self.is_fast,
                 _ => (),
             }
         }
